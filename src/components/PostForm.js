c9206bd5e191a7a50b69550b529c70e070294ae5
Celdama <celdama.dev@gmail.com>
2022-02-15 10:40:03 +0100

    add function to add post when form is submit in PostForm component

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -1,14 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
 import { addPost } from '../store/actions/postAction';
+import { usersSelector } from '../store/selectors/usersSelector';
 
 const PostForm = () => {
   const [formData, setFormData] = useState({
     title: '',
-    author: 'Celdama',
     content: '',
     likes: 0,
   });
+
+  const dispatch = useDispatch();
+  const users = useSelector(usersSelector);
 
   const handleChange = (e) => {
     const { name, value } = e.target;
@@ -22,6 +26,12 @@ const PostForm = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    dispatch(addPost({ ...formData, author: users[0].pseudo }));
+    setFormData({
+      title: '',
+      content: '',
+      likes: 0,
+    });
   };
 
   return (
@@ -40,7 +50,7 @@ const PostForm = () => {
           value={formData.content}
           placeholder='Postez vos pensées...'
         ></textarea>
-        <input onClick={(e) => handleSubmit()} type='submit' value='Envoyer' />
+        <input onClick={(e) => handleSubmit(e)} type='submit' value='Envoyer' />
       </form>
     </div>
   );
