4cd7d517da20e9f3899d1d84351c9a9854120e8b
Celdama <celdama.dev@gmail.com>
2022-02-15 09:05:23 +0100

    store: create post selector

--- src/store/index.js
+++ src/store/index.js
@@ -10,11 +10,13 @@
 import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import { postReducer } from './reducers/postReducer';
+import { userReducer } from './reducers/userReducer';
 import thunk from 'redux-thunk';
 
 const store = createStore(
   combineReducers({
     posts: postReducer,
+    users: userReducer,
   }),
   composeWithDevTools(applyMiddleware(thunk))
 );
